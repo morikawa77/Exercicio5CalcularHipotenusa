c515a1076e927b7fab98404dda0a19d61a7daf12
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-12 04:46:08 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio5CalcularHipotenusa
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			double baseT, altT, hipoT;
+			Console.WriteLine("Digite a base do triângulo: ");
+            baseT = Convert.ToDouble(Console.ReadLine());
+            baseT = Math.Pow(baseT, 2);
+			Console.WriteLine("Digite a altura do triângulo: ");
+			altT = Convert.ToDouble(Console.ReadLine());
+            altT = Math.Pow(altT, 2);
+            hipoT = baseT + altT;
+            hipoT = Math.Sqrt(hipoT);
+			Console.Write("A hipotenusa do triângulo é " + hipoT);
+			Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Exercício 5 - Calcular Hipotenusa de triângulo C# Curso Programação de Dispositivos Móveis SENAC
